01384a185b58dd0bee044439fbe4f98010552f7b
Pavel P <pavlov.pavel@gmail.com>
2024-05-27 03:22:12 +0200

    Always compile iccodec.c as part of libic
     + remove unused tpsizeset function

--- lib/include_/iccodec.h
+++ lib/include_/iccodec.h
@@ -133,7 +133,6 @@ size_t vhidecomp32x(const unsigned char* in, size_t inlen, unsigned char* out, s
 
 void tpmodeset(unsigned _tpmode);
 
-void tpsizeset(unsigned _tpsize);
 int  lzidget(const char *scmd);
 const char *codstr(unsigned cid);
 unsigned* getAvailableLzs(); // ICC_LAST will be the last entry
